0fccf85a37aaa18dbc8deffe05bca267773b3a06
Dmitry <df.creative@gmail.com>
2016-01-02 17:46:56 -0500

    Fix working version till it is not destroyed by refactoring

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var inherits = require('inherits');
 var css = require('mucss/css');
 var Draggable = require('draggy');
 var Resizable = require('resizable');
+var registry = require('./lib/registry');
 
 
 module.exports = Designer;
@@ -48,10 +49,11 @@ Designer.prototype.createDOM = function () {
 	self.audioElementsEl = document.createElement('div');
 	self.audioElementsEl.className = 'audio-elements';
 	self.element.appendChild(self.audioElementsEl);
+	self.audioElementsEl.setAttribute('title', 'Append new frequency');
 
 	//by click on the constructor - add new element
 	self.audioElementsEl.addEventListener('dblclick', function (e) {
-		var f = (e.offsetX/self.audioElementsEl.offsetWidth)*20000;
+		var f = w2f(e.offsetX, self.audioElementsEl.offsetWidth);
 
 		self.createAudioElement(f);
 	});
@@ -66,6 +68,24 @@ Designer.prototype.createDOM = function () {
 	//create spirallogram viewer
 
 }
+
+
+/**
+ * Update history to accord to the settings
+ */
+Designer.prototype.save = function () {
+	//serialize it css-like
+	//#a1 { source: saw, gain: n, quality: 0.1, frequency: n, detune: n, filter: ...}
+	//#a2 { source: url(#a1),  }
+};
+
+
+/**
+ * Load settings from passed url
+ */
+Designer.prototype.load = function (url) {
+	//parse it css-like
+};
 
 
 /**
@@ -84,33 +104,29 @@ Designer.prototype.createAudioElement = function (f) {
 	var audioElementEl = document.createElement('div');
 	audioElementEl.classList.add('audio-element');
 	self.audioElementsEl.appendChild(audioElementEl);
+	audioElementEl.setAttribute('title', 'Shift frequency');
 
 	//save audioElement on the element
 	audioElementEl.audioElement = audioElement;
-
-	//place audioEl so to reflect the frequency
-	var ratio = audioElement.frequency / audioElement.maxFrequency;
-	css(audioElementEl, {
-		// left: ratio * self.audioElementsEl.offsetWidth,
-		// width:
-	});
+	audioElementEl.setAttribute('frequency', audioElement.frequency.toFixed(0) + 'hz');
 
 	//make audioElement draggable
 	var draggable = new Draggable(audioElementEl, {
 		within: self.audioElementsEl,
-		threshold: 0
+		threshold: 0,
+		css3: false,
+		pin: [audioElementEl.offsetWidth / 2,0,audioElementEl.offsetWidth / 2, audioElementEl.offsetHeight ]
 	});
 
-	//freqs range of audio element
-	var fRange = (audioElement.maxFrequency - audioElement.minFrequency);
-
 	//set position acc to the frequency
-	draggable.move(ratio * self.audioElementsEl.offsetWidth, 0);
+	draggable.move(f2w(audioElement.frequency, self.audioElementsEl.offsetWidth), 0);
 
 	//update frequency on draggable being dragged
 	draggable.on('drag', function (e) {
 		var x = draggable.getCoords()[0];
-		var f = fRange * x / self.audioElementsEl.offsetWidth;
+		var f = w2f(x, self.audioElementsEl.offsetWidth);
+
+		audioElementEl.setAttribute('frequency', f.toFixed(0) + 'hz');
 
 		//regenerate frequency
 		//TODO: use logarithms here
@@ -125,13 +141,22 @@ Designer.prototype.createAudioElement = function (f) {
 		// within: 'parent',
 		handles: ['e', 'w']
 	});
+	resizable.handles.e.setAttribute('title', 'Change uncertainty');
+	resizable.handles.w.setAttribute('title', 'Change uncertainty');
 
 	resizable.on('resize', function () {
 		var w = audioElementEl.offsetWidth;
-		var ratio = w / self.audioElementsEl.offsetWidth;
-		var qRange = ratio * fRange;
+		var left = draggable.getCoords()[0];
 
-		audioElement.setQuality(qRange);
+		var range = audioElementEl.offsetWidth - 20;
+
+		var fLeft = w2f(left - range/2, self.audioElementsEl.offsetWidth);
+		var fRight = w2f(left + range/2, self.audioElementsEl.offsetWidth);
+
+		audioElement.setRange(fRight - fLeft);
+
+		//set draggable pin
+		draggable.pin = [audioElementEl.offsetWidth / 2,0,audioElementEl.offsetWidth / 2, audioElementEl.offsetHeight ];
 	})
 
 	//TODO: implement this using slidy - enhance and finish slidy
@@ -144,3 +169,28 @@ Designer.prototype.createAudioElement = function (f) {
 	// 	picker: q('.mixer-thumb', mixerTrackEl)
 	// });
 }
+
+
+var maxF = 20000, minF = 20;
+var decades = Math.round(lg(maxF/minF));
+var decadeOffset = lg(minF/10);
+
+
+/** 10 log */
+function lg (a) {
+	return Math.log(a) / Math.log(10);
+}
+
+
+/** Map frequency to an x coord */
+function f2w (f, w) {
+	var decadeW = w / decades;
+	return decadeW * (lg(f) - 1 - decadeOffset);
+};
+
+
+/** Map x coord to a frequency */
+function w2f (x, w) {
+	var decadeW = w / decades;
+	return Math.pow(10, x/decadeW + 1 + decadeOffset);
+};
--- questions.md
+++ questions.md
@@ -1,1 +1,7 @@
-*
+* Should I regenerate buffer source each time change took place?
+	* - it’s expensive and buggsome. For things like formant transformer, value, filter etc better use scriptProcessorNode, but keep source unchanged.
+	* + using scriptProcessor makes impossible referencing like `source: url(#a2)`, as #a2 should be generated already. Ideally - regenerate buffers per-sample (or missed number of samples per tick) to avoid too big refreshes.
+		* - Though it is able to save last buffer of scriptProcessorNode to provide to other guys while processing. We just have to make sure processors are called in proper order, and unfortunately we cannot affect it, as processor is independent shit. It is almost the same as per-sample handling.
+			* - Suppose, we can pregenerate buffers in webworkers, and let scriptProcessors just take copied chunk, right? To make interdependent generation possible. The question is responsiveness.
+* How to provide responsiveness?
+	* Ideally, just schedule user’s intention and prioritize it. Handle it in webworker, leave main thread for UX only.
